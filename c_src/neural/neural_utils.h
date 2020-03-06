04fcc5d48653b751552274155bcbcfd7de363e55
SisMaker <1713699517@qq.com>
2020-03-06 23:22:38 +0800

    nif调整

--- c_src/neural/neural_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef NEURAL_UTILS_H
-#define NEURAL_UTILS_H
-
-#include "erl_nif.h"
-#define WORD_SIZE sizeof(int)
-
-unsigned long int estimate_size(ErlNifEnv *env, ERL_NIF_TERM term);
-
-#endif
